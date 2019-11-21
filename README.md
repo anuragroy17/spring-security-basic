60b70f48ef6a72901ea9730c3191bd88f5d6ba5e
anuragroy17 <anuragroy17@gmail.com>
2019-11-21 11:30:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Spring Security Demo:
+- Basic Spring Security with role base login
+- Also has Lombok (So Install Lombok from https://projectlombok.org/ for your IDE)
+
+Tasks:-
+- Frontend in Thymeleaf currently(got it from a close source), needs conversion to Angular 8
+
+- http://localhost:8080/registration
+- http://localhost:8080/login
---
+++ src/main/java/com/anuragroy/login/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.anuragroy.login.repository;
+
+import com.anuragroy.login.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
